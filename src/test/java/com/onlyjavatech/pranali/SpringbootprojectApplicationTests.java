6462ab949640a18b362c0f165d629b4a01353e6d
Pranali132 <pranaliutt123@gmail.com>
2021-06-03 18:04:28 +0530

    Springboot

---
+++ src/test/java/com/onlyjavatech/pranali/SpringbootprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlyjavatech.pranali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
